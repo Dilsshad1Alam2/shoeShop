6bf46afbf9d19ace7a33370727ca2e89f20b9c4d
Dilshad <dilshad.alam3215@gmail.com>
2020-12-13 12:34:23 +0545

    sections added

---
+++ app.js
@@ -0,0 +1,20 @@
+const buttoonGoTop = document.querySelector('.sticky-go-top');
+
+
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+      buttoonGoTop.style.display = "block";
+    } else {
+      buttoonGoTop.style.display = "none";
+    }
+  }
+  
+  window.onscroll = function(){
+        scrollFunction()
+    };
+
+
+buttoonGoTop.addEventListener('click', function(){
+    console.log('clicked')
+})
