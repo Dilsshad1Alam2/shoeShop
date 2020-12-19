c1eace1647bffd27092ccddf8542fd120e463d35
Dilshad <dilshad.alam3215@gmail.com>
2020-12-19 10:42:15 +0545

    arrow fix

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const buttoonGoTop = document.querySelector('#up-arrow');
+const buttoonGoTop = document.querySelector('.sticky-go-top');
 const burgerMenu = document.querySelector('.fa-bars');
 const toHideInPhone = document.querySelector('.to-grab')
 
